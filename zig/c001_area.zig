68332aa280c36a78b47aa23e9db6977e5c12a621
killman2016 <george.zheng.china@gmail.com>
2024-02-22 20:45:46 +0800

    Update c001_area.zig

--- zig/c001_area.zig
+++ zig/c001_area.zig
@@ -1,32 +1,41 @@
 /// reference https://cstutorialpoint.com/c-programming-examples/
 ///
 const std = @import("std");
+
+//Most of the time, it is more appropriate to write to stderr
 const print = std.debug.print;
+
 const PI: f32 = 3.14;
 
 const Circle = struct {
     radius: f32 = undefined,
 
-    pub fn new(radius: f32) Circle {
+    fn new(radius: f32) Circle {
         return Circle{ .radius = radius };
     }
 
-    pub fn init(self: *Circle, radius: f32) void {
+    fn init(self: *Circle, radius: f32) void {
         self.*.radius = radius;
     }
 
-    pub fn getArea(self: Circle) f32 {
+    fn getArea(self: Circle) f32 {
         return PI * self.radius * self.radius;
     }
 
-    pub fn getCircleference(self: Circle) f32 {
+    fn getCircleference(self: Circle) f32 {
         return 2 * PI * self.radius;
     }
 
-    pub fn printMe(self: Circle) void {
-        print("Radius of Circle is : {d:.2}\n", .{self.radius});
-        print("Area of Circle is : {d:.2}\n", .{self.getArea()});
-        print("Circleference of Circle is : {d:.2}\n\n", .{self.getCircleference()});
+    fn printMe(self: Circle) !void {
+        // Write to stdout
+        const stdout = std.io.getStdOut().writer();
+        const strRadius = "Radius of Circle is";
+        const strArea = "Area of Circle is";
+        const strCircleference = "Circleference of Circle is";
+        const strFormat = "{s:>26} : {d:10.2}\n";
+        try stdout.print(strFormat, .{ strRadius, self.radius });
+        try stdout.print(strFormat, .{ strArea, self.getArea() });
+        try stdout.print(strFormat ++ "\n", .{ strCircleference, self.getCircleference() });
     }
 };
 
@@ -36,18 +45,32 @@ pub fn main() !void {
     const coc: f32 = 2 * PI * fr;
 
     var c = Circle.new(8.0);
-    c.printMe();
+    try c.printMe();
 
     var c1 = Circle{};
     c1.init(6.0);
-    c1.printMe();
+    try c1.printMe();
 
-    print("radius = {d:.2} area = {d:.2} circleference = {d:.2}\n", .{ fr, aoc, coc });
+    const strPrompt =
+        \\Radius        = {d:18.2}
+        \\Area          = {d:18.2}
+        \\Circleference = {d:18.2}
+    ;
+    print(strPrompt ++ "\n\n", .{ fr, aoc, coc });
 
     while (true) {
-        const ur: f32 = get_number(f32) catch continue;
-        if (ur == 0) break;
-        printArea(ur);
+        const radius: f32 = get_number(f32) catch |err| {
+            print("error2: {any}\n", .{err});
+            continue;
+        };
+
+        if (radius < 0) {
+            continue;
+        } else if (radius == 0) {
+            break;
+        } else {
+            printArea(radius);
+        }
     }
 }
 
@@ -62,9 +85,9 @@ fn printArea(radius: anytype) void {
 
     const aoc: f32 = PI * fr * fr;
     const coc: f32 = 2.0 * PI * fr;
-    print("Radius of Circle is : {d:.2}\n", .{radius});
-    print("Area of Circle is : {d:.2}\n", .{aoc});
-    print("Circleference of Circle is : {d:.2}\n\n", .{coc});
+    print("       Radius of Circle is : {d:18.2}\n", .{radius});
+    print("         Area of Circle is : {d:18.2}\n", .{aoc});
+    print("Circleference of Circle is : {d:18.2}\n\n", .{coc});
 }
 
 fn get_float32() !f32 {
@@ -72,8 +95,11 @@ fn get_float32() !f32 {
     // Adjust the buffer size depending on what length the input
     // will be or use "readUntilDelimiterOrEofAlloc"
     var buffer: [256]u8 = undefined;
-    print("Enter the radius: ", .{});
-    if (try stdin.readUntilDelimiterOrEof(buffer[0..], '\n')) |value| {
+
+    const strPrompt = "Enter the radius: ";
+    print("{s:>26} :", .{strPrompt});
+
+    if (try stdin.readUntilDelimiterOrEof(&buffer, '\n')) |value| {
         // We trim the line's contents to remove any trailing '\r' chars
         const line = std.mem.trimRight(u8, value[0 .. value.len - 1], "\r");
         return try std.fmt.parseFloat(f32, line);
@@ -85,16 +111,27 @@ fn get_float32() !f32 {
 // We can read any arbitrary number type with number_type
 fn get_number(comptime number_type: type) !number_type {
     const stdin = std.io.getStdIn().reader();
-    print("Enter the radius ({any}): ", .{number_type});
+    //print("Enter the radius ({any}): ", .{number_type});
+
+    const strPrompt = "Enter the radius";
+    print("{s} : ", .{strPrompt});
+
     // Adjust the buffer size depending on what length the input
     // will be or use "readUntilDelimiterOrEofAlloc"
-    var buffer: [256]u8 = undefined;
+    var buffer: [128]u8 = undefined;
 
-    // Read until the '\n' char and capture the value if there's no error
-    if (try stdin.readUntilDelimiterOrEof(buffer[0..], '\n')) |value| {
-        // We trim the line's contents to remove any trailing '\r' chars
-        const line = std.mem.trimRight(u8, value[0 .. value.len - 1], "\r");
-
+    if (stdin.readUntilDelimiterOrEof(&buffer, '\n') catch |err| switch (err) {
+        error.StreamTooLong => blk: {
+            try stdin.skipUntilDelimiterOrEof('\n');
+            break :blk &buffer;
+        },
+        else => |e| {
+            print("error1: {any}, {any}\n", .{ e, buffer });
+            return e;
+        },
+    }) |value| {
+        const line = std.mem.trimRight(u8, value[0 .. value.len - 1], "r");
+        print("line = {s}, value = {s}\n", .{ line, value });
         return switch (number_type) {
             f32 => try std.fmt.parseFloat(number_type, line),
             u32 => try std.fmt.parseInt(number_type, line, 10),
@@ -103,4 +140,17 @@ fn get_number(comptime number_type: type) !number_type {
     } else {
         return @as(number_type, 0);
     }
+    // // Read until the '\n' char and capture the value if there's no error
+    // if (try stdin.readUntilDelimiterOrEof(&buffer, '\n')) |value| {
+    //     // We trim the line's contents to remove any trailing '\r' chars
+    //     const line = std.mem.trimRight(u8, value[0 .. value.len - 1], "\r");
+    //
+    //     return switch (number_type) {
+    //         f32 => try std.fmt.parseFloat(number_type, line),
+    //         u32 => try std.fmt.parseInt(number_type, line, 10),
+    //         else => @as(number_type, 0),
+    //     };
+    // } else {
+    //     return @as(number_type, 0);
+    // }
 }
