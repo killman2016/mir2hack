04295f0241e8ec417fb3fe73dc82bad1361e5f1a
killman2016 <george.zheng.china@gmail.com>
2024-02-22 20:49:55 +0800

    Create c005_serialization.zig

---
+++ zig/c005_serialization.zig
@@ -0,0 +1,35 @@
+/// Custom JSON serialization in Zig
+/// https://www.aolium.com/karlseguin/46252c5b-587a-c419-be96-a0ccc2f11de4
+/// c005_serialization.zig
+const std = @import("std");
+
+const NumericBoolean = struct {
+    value: bool,
+
+    pub fn jsonStringify(self: NumericBoolean, out: anytype) !void {
+        const json: u8 = if (self.value) 1 else 0;
+        return out.write(json);
+    }
+};
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    //defer _ = gpa.deinit();
+    defer std.testing.expect(gpa.deinit() == .ok) catch @panic("leak");
+
+    const allocator = gpa.allocator();
+
+    var arr = std.ArrayList(u8).init(allocator);
+    defer arr.deinit();
+
+    try std.json.stringify(.{
+        .accept = NumericBoolean{ .value = true },
+    }, .{}, arr.writer());
+
+    std.debug.print("{s}\n", .{arr.items});
+
+    //Outputting: {"accept":1}.
+    try std.testing.expect(std.mem.eql(u8, arr.items, "{\"accept\":1}"));
+    //std.testing.print("{}\n", .{mem.eql(u8, "hello", "h\x65llo")});      // true
+    //std.testing.print("{}\n", .{mem.eql(u8, "💯", "\xf0\x9f\x92\xaf")}); // also true
+}
